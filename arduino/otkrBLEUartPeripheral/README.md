97de958cd6ef4c3ffd87529e17222e739d345663
Alex Makhlin <amakhlin@gmail.com>
2021-12-05 13:22:37 -0600

    adding arduino code

---
+++ arduino/otkrBLEUartPeripheral/README.md
@@ -0,0 +1,8 @@
+# Arduino board info
+This code runs on Adafruit Feather nRF52 Bluefruit LE - nRF52832, more info is [here](https://www.adafruit.com/product/3406?gclid=Cj0KCQiA47GNBhDrARIsAKfZ2rBb9XEdMtHPNB3V-s9d2wVGZU6RoBF-IGlw2pdKrWL_ai1gvkb1AGoaAnD5EALw_wcB)
+
+# BSP
+Requires Adafruit's [BSP](https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/arduino-bsp-setup)
+
+# Library
+Uses Adafruit's nRF52 (Nordic Semiconductor) [library](https://github.com/adafruit/Adafruit_nRF52_Arduino/tree/master/libraries/Bluefruit52Lib/examples)
---
+++ arduino/otkrBLEUartPeripheral/otkrBLEUartPeripheral.ino
@@ -0,0 +1,201 @@
+/*********************************************************************
+ This is an example for our nRF52 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <bluefruit.h>
+
+// OTA DFU service
+BLEDfu bledfu;
+
+// Uart over BLE service
+BLEUart bleuart;
+
+// Function prototypes for packetparser.cpp
+uint8_t readPacket (BLEUart *ble_uart, uint16_t timeout);
+float   parsefloat (uint8_t *buffer);
+void    printHex   (const uint8_t * data, const uint32_t numBytes);
+
+//SoftwareTimer postAutounlockTimer;
+//bool inPostAutounlockTimeout = false;
+
+// Packet buffer
+extern uint8_t packetbuffer[];
+
+void setup(void)
+{
+  
+  pinMode(17, OUTPUT);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, LOW);
+
+  //postAutounlockTimer.begin(10000, post_unlock_timer_callback, NULL, false); // non-repeating
+  
+  Serial.begin(115200);
+  while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+  Bluefruit.begin();
+
+  //Supported tx_power values depending on mcu:
+  // - nRF52832: -40dBm, -20dBm, -16dBm, -12dBm, -8dBm, -4dBm, 0dBm, +3dBm and +4dBm.
+  // -20dbm is 2-3 feet of line of site distance
+  
+  Bluefruit.setTxPower(-4);    // Check bluefruit.h for supported values
+
+  Bluefruit.autoConnLed(false);
+
+  //Bluefruit.Periph.setConnectCallback(connect_callback);
+  //Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+
+  // To be consistent OTA DFU should be added first if it exists
+  bledfu.begin();
+
+  // Configure and start the BLE Uart service
+  bleuart.begin();
+
+  // Set up and start advertising
+  startAdv();
+}
+
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  
+  // Include the BLE UART (AKA 'NUS') 128-bit UUID
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 32);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
+{
+  // Wait for new data to arrive
+  uint8_t len = readPacket(&bleuart, 500);
+  if (len == 0) return;
+
+  // Got a packet!
+  printHex(packetbuffer, len);
+
+  // Buttons
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttnum = packetbuffer[2] - '0';
+    boolean unlockCmd = packetbuffer[3] - '0';
+    Serial.print ("Button "); Serial.print(buttnum);
+    if (unlockCmd == 1) { // auto unlock command
+      //if(!inPostAutounlockTimeout){
+        //postAutounlockTimer.start();
+        //inPostAutounlockTimeout = true;
+        
+        digitalWrite(17, HIGH);
+        digitalWrite(16, HIGH);
+
+        // respond
+        uint8_t buf[10];
+        buf[0] = 0x55;
+        bleuart.write( buf, 1 );
+  
+        delay(3000);
+
+        digitalWrite(17, LOW);
+        digitalWrite(16, LOW);
+     }
+      /*else{
+        // respond
+        uint8_t buf[10];
+        buf[0] = 0x55;
+        bleuart.write( buf, 1 );
+      }*/
+    /*} else { // immediate unlock command
+        postAutounlockTimer.start();
+        inPostAutounlockTimeout = true;
+        
+        digitalWrite(17, HIGH);
+        digitalWrite(16, HIGH);
+
+        // respond
+        uint8_t buf[10];
+        buf[0] = 0x55;
+        bleuart.write( buf, 1 );
+  
+        delay(1000);
+
+        digitalWrite(17, LOW);
+        digitalWrite(16, LOW);
+    }*/
+  }
+
+}
+
+
+
+/*
+// callback invoked when central connects
+void connect_callback(uint16_t conn_handle)
+{
+  // Get the reference to current connection
+  //BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  //char central_name[32] = { 0 };
+  //connection->getPeerName(central_name, sizeof(central_name));
+
+  //Serial.print("Connected to ");
+  //Serial.println(central_name);
+  Bluefruit.Advertising.stop(); 
+  
+}
+*/
+/*
+void post_unlock_timer_callback(TimerHandle_t xTimerID){
+  inPostAutounlockTimeout = false;
+}
+*/
+/*void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  //(void) conn_handle;
+  //(void) reason;
+
+  //if(unlockComplete){
+
+    // wait for a while before allowing another unlock
+  //  delay(10000);
+   // unlockComplete = false;
+    
+  //}
+
+  Bluefruit.Advertising.start(0); 
+
+  //Serial.println();
+  //Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
+}*/
